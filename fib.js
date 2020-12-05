1f47c9d4ff878521005f0a25e2653618c8fd6c60
tosebikan <tosebikan@gmail.com>
2020-12-05 22:29:53 +0000

    using memoization

--- fib.js
+++ fib.js
@@ -1,10 +1,25 @@
-const fib = n => {
+// const fib = n => {
+//   if (n <= 2) return 1;
+//
+//   return fib(n - 1) + fib(n - 2);
+// };
+//
+// console.log(fib(2));
+// console.log(fib(6));
+// console.log(fib(8));
+// console.log(fib(40));
+
+// using memoization
+
+const fib = (n, memo = {}) => {
+  if (n in memo) return memo[n];
   if (n <= 2) return 1;
 
-  return fib(n - 1) + fib(n - 2);
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
 };
 
 console.log(fib(2));
 console.log(fib(6));
 console.log(fib(8));
-console.log(fib(40));
+console.log(fib(50));
